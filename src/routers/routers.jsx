defc26dc5dd5de040f48514b68c34ffaeca87e18
skmajumder <skmajumder.cse@gmail.com>
2023-11-03 17:44:13 +0600

    handeling error

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,47 +1,44 @@
-import { createBrowserRouter } from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 
 import Home from "../ui/Home";
-import NotFound from "../ui/Error";
+import Error from "../ui/Error";
 import Menu from "../features/menu/Menu";
 import Cart from "../features/cart/Cart";
 import CreateOrder from "../features/order/CreateOrder";
 import Order from "../features/order/Order";
 import AppLayout from "../ui/AppLayout";
-import { menuLoader } from "../loaders/loaderFunction";
+import {menuLoader} from "../loaders/loaderFunction";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      {
+    {
         path: "/",
-        element: <Home />,
-        errorElement: <NotFound />,
-      },
-      {
-        path: "/menu",
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <NotFound />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-        errorElement: <NotFound />,
-      },
-      {
-        path: "/order/new",
-        element: <CreateOrder />,
-        errorElement: <NotFound />,
-      },
-      {
-        path: "/order/:orderID",
-        element: <Order />,
-        errorElement: <NotFound />,
-      },
-    ],
-  },
+        element: <AppLayout/>,
+        errorElement: <Error/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>,
+            },
+            {
+                path: "/menu",
+                element: <Menu/>,
+                loader: menuLoader,
+                errorElement: <Error/>,
+            },
+            {
+                path: "/cart",
+                element: <Cart/>,
+            },
+            {
+                path: "/order/new",
+                element: <CreateOrder/>,
+            },
+            {
+                path: "/order/:orderID",
+                element: <Order/>,
+            },
+        ],
+    },
 ]);
 
 export default router;
