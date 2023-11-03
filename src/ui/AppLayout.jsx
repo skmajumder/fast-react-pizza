9aad0f23bd121692c69268629cbbdc0eed50c8ce
skmajumder <skmajumder.cse@gmail.com>
2023-11-03 17:18:10 +0600

    loading icon by useNavigation hook

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,18 +1,23 @@
-import { Outlet } from "react-router-dom";
+import {Outlet, useNavigation} from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader.jsx";
 
 const AppLayout = () => {
-  return (
-    <div>
-      <Header />
-      <Main>
-        <Outlet />
-      </Main>
-      <CartOverview />
-    </div>
-  );
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading"
+
+    return (
+        <div className={'layout'}>
+            {isLoading && <Loader/>}
+            <Header/>
+            <Main>
+                <Outlet/>
+            </Main>
+            <CartOverview/>
+        </div>
+    );
 };
 
 export default AppLayout;
