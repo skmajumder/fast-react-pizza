886fbf338f48cd8b57d250d9f72c1f5024cdc6cf
skmajumder <skmajumder.cse@gmail.com>
2023-11-04 13:49:37 +0600

    tailwind color

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,42 +1,42 @@
-import { createBrowserRouter } from "react-router-dom";
-import { menuLoader, orderLoader } from "../loaders/loaderFunction";
-import { createOrderAction } from "../actions/actions";
+import { createBrowserRouter } from 'react-router-dom';
+import { menuLoader, orderLoader } from '../loaders/loaderFunction';
+import { createOrderAction } from '../actions/actions';
 
-import Home from "../ui/Home";
-import Error from "../ui/Error";
-import Menu from "../features/menu/Menu";
-import Cart from "../features/cart/Cart";
-import CreateOrder from "../features/order/CreateOrder";
-import Order from "../features/order/Order";
-import AppLayout from "../ui/AppLayout";
+import Home from '../ui/Home';
+import Error from '../ui/Error';
+import Menu from '../features/menu/Menu';
+import Cart from '../features/cart/Cart';
+import CreateOrder from '../features/order/CreateOrder';
+import Order from '../features/order/Order';
+import AppLayout from '../ui/AppLayout';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <AppLayout />,
     errorElement: <Error />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
       {
-        path: "/cart",
+        path: '/cart',
         element: <Cart />,
       },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: "/order/:orderID",
+        path: '/order/:orderID',
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
