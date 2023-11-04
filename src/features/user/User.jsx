8b2ccb772372120ba224de630462aa85b9f018ba
skmajumder <skmajumder.cse@gmail.com>
2023-11-04 21:43:27 +0600

    font size

---
+++ src/features/user/User.jsx
@@ -0,0 +1,9 @@
+const User = () => {
+    return (
+        <div className="text-sm font-semibold">
+            Test
+        </div>
+    );
+};
+
+export default User;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
-import {Link} from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder.jsx";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder.jsx';
+import User from '../features/user/User.jsx';
 
 const Header = () => {
-    return (
-        <header className="bg-yellow-500">
-            <Link to={"/"}>Fast React Pizza</Link>
-            <SearchOrder/>
-            <p>TEST Name</p>
-        </header>
-    );
+  return (
+    <header className="bg-yellow-500 uppercase">
+      <Link to={'/'} className="tracking-widest">
+        Fast React Pizza
+      </Link>
+      <SearchOrder />
+      <User />
+    </header>
+  );
 };
 
 export default Header;
