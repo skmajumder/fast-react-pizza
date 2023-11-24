8d28cf4d51e92db6c2f2d89168c87952457332c9
skmajumder <skmajumder.cse@gmail.com>
2023-11-25 01:58:44 +0600

    new item in cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,24 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utilities/helpers';
+import { addItem } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
-  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const handleAddToCart = function () {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice: 16,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+  };
 
   return (
     <li className="flex gap-4 py-2">
@@ -24,7 +40,9 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+          <Button type="small" disabled={soldOut} onClick={handleAddToCart}>
+            {soldOut ? 'Not available' : 'Add to cart'}
+          </Button>
         </div>
       </div>
     </li>
